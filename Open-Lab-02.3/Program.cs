8f36e8c741636875788c9ad0d2f614c0e56ca09a
romankalab <roan.kalab@gmail.com>
2019-10-14 10:04:30 +0200

    Initial

---
+++ Open-Lab-02.3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace skuška
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Zadajte číslo: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Zadajte druhé číslo: ");
+            int number2 = Convert.ToInt32(Console.ReadLine());
+
+            int result = Remainder(number, number2);
+            Console.WriteLine($"Toto je zvyšok z delenia: {result}");
+        }
+        private static int Remainder(int number, int number2)
+        {
+            return number % number2;
+        }
+
+
+
+    }
+}
